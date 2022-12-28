0fb2a09ca9805c0a132c068de34c64c5fbce4f6d
mhaimaiimai <33171577+mhaimaiimai@users.noreply.github.com>
2022-12-28 19:30:46 +0700

    Improve code by 1) enhancing shifting range without IndexOutOfRange 2) rerun program automatically 3) avoid error from number input

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,33 +1,32 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-def encrypt(text, shift):
-    text_encrypted = ""
+           
+def CaesarCipher(text, shift, direction):
+    ciphered_text = ""
     for character in text:
-        character_ind = alphabet.index(character)
-        if character_ind+shift > len(alphabet)-1:
-            character_ind = character_ind - (len(alphabet))
-        text_encrypted += alphabet[character_ind+shift]
-    return text_encrypted
+        if character in alphabet:
+            character_ind = alphabet.index(character)
+            if direction == "encode":
+                if character_ind+shift > len(alphabet)-1:
+                    character_ind = character_ind - (len(alphabet)) #avoid index out of range
+                ciphered_text += alphabet[character_ind+shift]
+            else:
+                if character_ind-shift < 0:
+                    character_ind = character_ind + (len(alphabet)) #avoid index out of range
+                ciphered_text += alphabet[character_ind-shift]
+        else:
+            ciphered_text += character
+    print(f"{direction}d text is: {ciphered_text}")
 
-def decrypt(text, shift):
-    text_decoded = ""
-    for character in text:
-        character_ind = alphabet.index(character)
-        if character_ind-shift < 0:
-            character_ind = character_ind + (len(alphabet))
-        text_decoded += alphabet[character_ind-shift]
-    return text_decoded
-        
-def CaesarCipher():
+run_cipher = True
+while(run_cipher):
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
+    shift = shift%len(alphabet) #avoid index out of range
     
-    if direction == "encode":
-        text_encrypted = encrypt(text, shift)
-        print(f"Encrypted text: {text_encrypted}")
-    else:
-        text_decoded = decrypt(text, shift)
-        print(f"Decrypted text: {text_encrypted}")
-
-CaesarCipher()
+    CaesarCipher(text, shift, direction)
+    
+    if input("Type 'yes' if you want to go again. Otherwise, type 'no': ").lower() == "yes": 
+        run_cipher=True 
+    else: 
+        run_cipher=False
